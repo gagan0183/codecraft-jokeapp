d511f01d3f954abc8020400494f09144dbeac7b7
Gagan <g.deepsingh1@gmail.com>
2018-07-08 15:26:12 +0530

    joke app start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeListComponent
   ],
   imports: [
     BrowserModule
